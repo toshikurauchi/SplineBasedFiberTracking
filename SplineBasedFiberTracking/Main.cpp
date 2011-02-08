dfb50f16317a6e30f35e5e59c45e6e81559b9bce
unknown <Toshi@.(none)>
2011-02-08 12:39:01 -0200

    Reading field from file

--- SplineBasedFiberTracking/Main.cpp
+++ SplineBasedFiberTracking/Main.cpp
@@ -1,6 +1,7 @@
 
 
 #include <iostream>
+#include <fstream>
 
 #include "pbge/core/Manager.h"
 #include "pbge/gfx/Light.h"
@@ -18,21 +19,44 @@ int cam_node_name;
 pbge::Renderer * renderer;
 pbge::SceneGraph * scene;
 
+void createVectorFieldFromFile(pbge::SceneGraph *scene) {
+    std::ifstream inputFile("inputField.txt", std::ifstream::in);
+    
+    int numberOfVectors = 0;
+    inputFile >> numberOfVectors;
+    while(numberOfVectors--) {
+        int pos_x, pos_y, pos_z;
+        int vec_x, vec_y, vec_z;
+        inputFile >> pos_x;
+        inputFile >> pos_y;
+        inputFile >> pos_z;
+        inputFile >> vec_x;
+        inputFile >> vec_y;
+        inputFile >> vec_z;
+
+        math3d::vector4 pos = math3d::vector4(pos_x,pos_y,pos_z);
+        math3d::vector4 vec = math3d::vector4(vec_x,vec_y,vec_z);
+
+        pbge::ModelInstance * vectorModel = Vector(pos, vec).createVectorInstance();
+    
+        pbge::Node * vectorPosNode = scene->appendChildTo(pbge::SceneGraph::ROOT, pbge::TransformationNode::translation(pos[0],pos[1],pos[2]));
+        scene->appendChildTo(vectorPosNode, vectorModel);
+    }
+    inputFile.close();
+}
+
 void setUp() {
     pbge::Manager::init();
     pbge::Manager::getInstance()->getOpenGL()->enableMode(pbge::OpenGL::DEPTH_TEST);
     glClearColor(0,0,0,0);
-    math3d::vector4 pos = math3d::vector4(0,0,1);
-    math3d::vector4 vec = math3d::vector4(1,1,0);
-    pbge::ModelInstance * vboModel = Vector(pos, vec).createVectorInstance();
 
     renderer = new pbge::Renderer(pbge::Manager::getInstance()->getOpenGL());
     scene = new pbge::SceneGraph(new pbge::TransformationNode);
 
-    pbge::Node * vectorPosNode = scene->appendChildTo(pbge::SceneGraph::ROOT, pbge::TransformationNode::translation(pos[0],pos[1],pos[2]));
-    cam_node_name = scene->appendChildTo(pbge::SceneGraph::ROOT, pbge::TransformationNode::translation(0, 0, 3))->getSceneGraphIndex();
+    createVectorFieldFromFile(scene);
+
+    cam_node_name = scene->appendChildTo(pbge::SceneGraph::ROOT, pbge::TransformationNode::translation(0, 0, 8))->getSceneGraphIndex();
     
-    scene->appendChildTo(vectorPosNode, vboModel);
     pbge::CameraNode * cam = dynamic_cast<pbge::CameraNode*>(scene->appendChildTo(cam_node_name, new pbge::CameraNode()));
     scene->appendChildTo(cam_node_name, new pbge::PointLight);
     cam->lookAt(math3d::vector4(0,1,0),math3d::vector4(0,0,-1));
